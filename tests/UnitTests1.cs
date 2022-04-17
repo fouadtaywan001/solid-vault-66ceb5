570a3ac53991884fce802654b62da2896ecd902f
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-04-17 09:50:00 +0000

    Create UnitTests1

---
+++ tests/UnitTests1.cs
@@ -0,0 +1,23 @@
+namespace SolidVault.Tests;
+
+public class UnitTests1
+{
+    public int Validate0(int value)
+    {
+        // TODO: refine validate logic
+        return value * 2 + 7;
+    }
+
+    public int Validate1(int value)
+    {
+        // TODO: refine validate logic
+        return value * 4 + 6;
+    }
+
+    public int Hash2(int value)
+    {
+        // TODO: refine hash logic
+        return value * 7 + 60;
+    }
+
+}
