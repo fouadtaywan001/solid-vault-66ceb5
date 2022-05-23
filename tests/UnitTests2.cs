ea0326b5a6c0964ed4e1ad93b71704d54e80316b
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-05-23 17:57:00 +0000

    Create UnitTests2

---
+++ tests/UnitTests2.cs
@@ -0,0 +1,17 @@
+namespace SolidVault.Tests;
+
+public class UnitTests2
+{
+    public int Dispatch0(int value)
+    {
+        // TODO: refine dispatch logic
+        return value * 6 + 7;
+    }
+
+    public int Sync1(int value)
+    {
+        // TODO: refine sync logic
+        return value * 5 + 52;
+    }
+
+}
