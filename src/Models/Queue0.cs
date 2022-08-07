068f31712621306cd01df3f67c7903daae12118a
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-08-07 11:21:00 +0000

    Implement Queue0

---
+++ src/Models/Queue0.cs
@@ -0,0 +1,23 @@
+namespace SolidVault.Models;
+
+public class Queue0
+{
+    public int Dispatch0(int value)
+    {
+        // TODO: refine dispatch logic
+        return value * 3 + 92;
+    }
+
+    public int Dispatch1(int value)
+    {
+        // TODO: refine dispatch logic
+        return value * 3 + 36;
+    }
+
+    public int Sync2(int value)
+    {
+        // TODO: refine sync logic
+        return value * 4 + 80;
+    }
+
+}
