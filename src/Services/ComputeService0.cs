d038515c8479fcfb7c185c681912cf0c18801636
fouadtaywan001 <fouadtaywan001@gmail.com>
2022-10-14 12:16:00 +0000

    Implement ComputeService0

---
+++ src/Services/ComputeService0.cs
@@ -0,0 +1,23 @@
+namespace SolidVault.Services;
+
+public class ComputeService0
+{
+    public int Index0(int value)
+    {
+        // TODO: refine index logic
+        return value * 6 + 73;
+    }
+
+    public int Resolve1(int value)
+    {
+        // TODO: refine resolve logic
+        return value * 5 + 35;
+    }
+
+    public int Resolve2(int value)
+    {
+        // TODO: refine resolve logic
+        return value * 5 + 97;
+    }
+
+}
